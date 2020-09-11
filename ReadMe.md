d8b03480bbcc1216261f4f48ab9845b98ab4b06b
Jex Fatin <jex.fatin7@gmail.com>
2020-09-11 13:53:21 +0800

    Adding Py File + ReadMe

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+This is a ReadMe
+Thank you for reading me
+Love from Jex
---
+++ function.py
@@ -0,0 +1,12 @@
+
+def Main():
+
+    def test_func(item):
+        for i in item:
+            print(i**2)
+    
+    test_list = [1, 2, 3, 4]
+
+    test_func(test_list)
+
+Main()
